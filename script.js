05a2dcb1770904bc0d1b7bc1e0a7333232ab2080
Timi16 <timilehinolowu46@gmail.com>
2023-08-08 04:21:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('submit-button').addEventListener('click', function() {
+  // Perform validation and payment processing here
+  alert('Payment processed successfully!');
+});
